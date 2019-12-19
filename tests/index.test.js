fadb095ced5dac154aac10b606e585a40beda286
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-12-19 21:14:17 +0100

    added test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,15 @@
-import csvInjectionProtector from "../src/index";
+import { csvInjectionProtector } from "../src/csvInjectionProtector";
 
 test("test '=' sign CSV sanitization", () => {
   const testStr = "=Risky string for CSV";
+  const expectedStr = "Risky string for CSV";
+  const sanitizedStr = csvInjectionProtector(testStr);
+
+  expect(sanitizedStr).toBe(expectedStr);
+});
+
+test("test '@' sign CSV sanitization", () => {
+  const testStr = "@Risky string for CSV";
   const expectedStr = "Risky string for CSV";
   const sanitizedStr = csvInjectionProtector(testStr);
 
