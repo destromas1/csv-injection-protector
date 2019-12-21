226999abc11bacb5b1d184b4381c449e30a96b2b
Ueli Banholzer <ueli@whatwedo.ch>
2019-12-21 19:33:36 +0100

    simplify existing code

--- src/csvInjectionProtector.js
+++ src/csvInjectionProtector.js
@@ -1,16 +1,7 @@
-import riskyChars from "./riskyChars";
-
 const csvInjectionProtector = str => {
     if (!str) return "";
 
-    const firstChar = str.charAt(0);
-    const isInjected = riskyChars.includes(firstChar);
-
-    if(!isInjected) return str;
-
-    const slicedStr = str.slice(1);
-
-    return csvInjectionProtector(slicedStr);
+    return str.replace(/^[=+-@]+(.*)/g, "$1");
 };
 
 export { csvInjectionProtector };
--- src/riskyChars.js
+++
@@ -1,3 +0,0 @@
-const riskyChars = ["=", "+", "-", "@"];
-
-export default riskyChars;
