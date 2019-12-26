325831677da12df9b366e66777429eeb059050b0
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-12-26 14:52:29 +0100

    test if double risky chars == sanitized

--- tests/index.test.js
+++ tests/index.test.js
@@ -23,3 +23,11 @@ test("test if double risky chars @@ sanitized", () => {
 
   expect(sanitizedStr).toBe(expectedStr);
 });
+
+test("test if double risky chars == sanitized", () => {
+  const testStr = "==Risky string for CSV";
+  const expectedStr = "Risky string for CSV";
+  const sanitizedStr = csvInjectionProtector(testStr);
+
+  expect(sanitizedStr).toBe(expectedStr);
+});
